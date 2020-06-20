852094a4650d0b734d8b08a014438e5254a591f0
Vellyus <53130942+Vellyus@users.noreply.github.com>
2020-06-20 22:42:18 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -161,27 +161,42 @@ console.log(video);
 
 // https://stackoverflow.com/questions/7853904/how-to-detect-when-a-youtube-video-finishes-playing
 
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
 var player;
-function onYouTubePlayerAPIReady() {
-    player = new YT.Player('player', {
-      // width: '640',
-      // height: '390',
-      videoId: '0Bmhjf0rKe8',
-      events: {
-        onReady: onPlayerReady,
-        onStateChange: onPlayerStateChange
-      }
-    });
-}
-
-// autoplay video
-function onPlayerReady(event) {
-    event.target.playVideo();
-}
-
-// when video ends
-function onPlayerStateChange(event) {        
-    if(event.data === 0) {          
-        alert('done');
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    height: '390',
+    width: '640',
+    videoId: 'M7lc1UVf-VE',
+    events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
     }
+  });
+}
+
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+  event.target.playVideo();
+}
+
+// 5. The API calls this function when the player's state changes.
+//    The function indicates that when playing a video (state=1),
+//    the player should play for six seconds and then stop.
+var done = false;
+function onPlayerStateChange(event) {
+  if (event.data == YT.PlayerState.PLAYING && !done) {
+    setTimeout(stopVideo, 6000);
+    done = true;
+  }
+}
+function stopVideo() {
+  player.stopVideo();
 }
