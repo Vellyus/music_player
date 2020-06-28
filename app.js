755d46263c3e922dec52e9d50fdd1d52eb3775af
Vellyus <53130942+Vellyus@users.noreply.github.com>
2020-06-28 19:32:54 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -388,7 +388,7 @@ document.querySelector('#invisiblePlayer').style.display = "none";
 
 
 function playNextSong() {
-  if (player.getCurrentTime != 0) {
+  if (player.getCurrentTime > 0) {
     if (player.getCurrentTime() === player.getDuration()) {
       // if (Math.ceil(player.getCurrentTime()) === Math.ceil(player.getDuration()) && playList.length === 0) {
       //   player.loadVideoById(urlToID2(urlToID(todaysSong)));
