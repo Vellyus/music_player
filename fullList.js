a400ca629eeade98e723573207dca9fdbcbd3070
Vellyus <53130942+Vellyus@users.noreply.github.com>
2020-07-20 16:21:15 +0200

    Add files via upload

--- fullList.js
+++ fullList.js
@@ -898,7 +898,7 @@ let myTimer3 = setInterval(muteInvisiblePlayer, 200);
 
 let myTimer = setInterval(playNextSong, 10000);
 
-let myTimer4 = setInterval(searchList, 1000);
+let myTimer4 = setInterval(searchList, 2000);
 
 
 // EXPERIMENT
